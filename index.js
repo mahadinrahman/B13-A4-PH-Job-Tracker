64abcb298e90feeaadcba95475ea669913dc9972
mahadinrahman <mahadinrahman333@gmail.com>
2026-02-26 01:10:25 +0600

    toggle add

--- index.js
+++ index.js
@@ -246,5 +246,15 @@ let interviewSection=document.getElementById('interview');
 
   });
 
+  const buttons=document.querySelectorAll('.toggle-btn');
+
+  buttons.forEach(button=>{
+    button.addEventListener('click',function(){
+
+        buttons.forEach(btn=>btn.classList.remove('active'));
+        this.classList.add('active');
+    });
+  });
+
   
 
